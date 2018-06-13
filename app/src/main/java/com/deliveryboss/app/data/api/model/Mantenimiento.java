3d9887aee3a8d12a97b6e76c7fad0ad4aacf37dd
Joaquin Arias Simone <juaco1993@gmail.com>
2018-06-12 21:29:39 -0300

    -Cambios en tabla mantenimiento. Ahora se llama "modo_mantenimiento". Modificacion de Modelo en app correspondiente.

--- app/src/main/java/com/deliveryboss/app/data/api/model/Mantenimiento.java
+++ app/src/main/java/com/deliveryboss/app/data/api/model/Mantenimiento.java
@@ -5,24 +5,24 @@ package com.deliveryboss.app.data.api.model;
  */
 
 public class Mantenimiento {
-    String idmantenimiento;
+    String idmodo_mantenimiento;
     String estado;         // 0=inactivo     1=activo
     String titulo;
     String mensaje;
 
-    public Mantenimiento(String idmantenimiento, String estado, String titulo, String mensaje) {
-        this.idmantenimiento = idmantenimiento;
+    public Mantenimiento(String idmodo_mantenimiento, String estado, String titulo, String mensaje) {
+        this.idmodo_mantenimiento = idmodo_mantenimiento;
         this.estado = estado;
         this.titulo = titulo;
         this.mensaje = mensaje;
     }
 
-    public String getIdmantenimiento() {
-        return idmantenimiento;
+    public String getIdmodo_mantenimiento() {
+        return idmodo_mantenimiento;
     }
 
-    public void setIdmantenimiento(String idmantenimiento) {
-        this.idmantenimiento = idmantenimiento;
+    public void setIdmodo_mantenimiento(String idmodo_mantenimiento) {
+        this.idmodo_mantenimiento = idmodo_mantenimiento;
     }
 
     public String getEstado() {
